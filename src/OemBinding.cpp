8d431cd20e85fe7ed8a461261588e5300fb6a33a
Sumanth Bhat <sumanth.bhat@intel.com>
2022-05-16 12:42:08 +0530

    Add binding properties (#5)
    This just adds the relevant binding properties.
    
    Tested: Verified that the added properties are
    exposed on D-Bus
    
    Signed-off-by: Sumanth Bhat <sumanth.bhat@intel.com>

--- src/OemBinding.cpp
+++ src/OemBinding.cpp
@@ -6,6 +6,9 @@ std::string mctpBindSMBus = "xyz.openbmc_project.MCTP.Binding.SMBus";
 std::string mctpBindPCIe = "xyz.openbmc_project.MCTP.Binding.PCIe";
 std::string mctpBindOem = "xyz.openbmc_project.MCTP.Binding.OEM";
 std::string path = "/dev/i2c-8";
+uint8_t arpMasterSupport = 0;
+uint8_t bmcSlaveAddress = 0x12;
+std::string pcieDiscoveredFlag = "xyz.openbmc_project.MCTP.Binding.PCIe.DiscoveryFlags.Discovered";
 
 OemBinding::OemBinding(
     std::shared_ptr<sdbusplus::asio::object_server>& objServer,
@@ -15,11 +18,14 @@ OemBinding::OemBinding(
     if (bind == bindType::smbus)
     {
         dbusInterface = objServer->add_interface(objPath, mctpBindSMBus);
+        dbusInterface->register_property("ArpMasterSupport", arpMasterSupport);
+        dbusInterface->register_property("BmcSlaveAddress", bmcSlaveAddress);
+        dbusInterface->register_property("BusPath", path);
     }
     else if (bind == bindType::pcie)
     {
         dbusInterface = objServer->add_interface(objPath, mctpBindPCIe);
-        dbusInterface->register_property("BusPath", path);
+        dbusInterface->register_property("DiscoveredFlag", pcieDiscoveredFlag);
         dbusInterface->register_property("BDF", 512);
     }
     else
